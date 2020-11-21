721f70f588885ab517fd6dc3c9338f61d91df257
Ali <62004172+ali2004-linux@users.noreply.github.com>
2020-11-21 00:03:42 -0800

    Add project files.

---
+++ Media Player/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace Media_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.btn_pre = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.btn_rm = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.btn_mou = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(2, 3);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(1014, 494);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(17, 565);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(75, 23);
+            this.btn_open.TabIndex = 1;
+            this.btn_open.Text = "انتخاب فایل";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(342, 565);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 2;
+            this.btn_stop.Text = "قطع";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.Location = new System.Drawing.Point(229, 565);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(75, 23);
+            this.btn_pause.TabIndex = 3;
+            this.btn_pause.Text = "متوقف";
+            this.btn_pause.UseVisualStyleBackColor = true;
+            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
+            // 
+            // btn_play
+            // 
+            this.btn_play.Location = new System.Drawing.Point(123, 565);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(75, 23);
+            this.btn_play.TabIndex = 4;
+            this.btn_play.Text = "پخش";
+            this.btn_play.UseVisualStyleBackColor = true;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+            // 
+            // btn_pre
+            // 
+            this.btn_pre.Location = new System.Drawing.Point(456, 565);
+            this.btn_pre.Name = "btn_pre";
+            this.btn_pre.Size = new System.Drawing.Size(75, 23);
+            this.btn_pre.TabIndex = 5;
+            this.btn_pre.Text = "قبلی";
+            this.btn_pre.UseVisualStyleBackColor = true;
+            this.btn_pre.Click += new System.EventHandler(this.btn_pre_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(572, 565);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(75, 23);
+            this.btn_next.TabIndex = 6;
+            this.btn_next.Text = "بعدی";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // btn_rm
+            // 
+            this.btn_rm.Location = new System.Drawing.Point(688, 565);
+            this.btn_rm.Name = "btn_rm";
+            this.btn_rm.Size = new System.Drawing.Size(113, 23);
+            this.btn_rm.TabIndex = 7;
+            this.btn_rm.Text = "حذف پلی لیست";
+            this.btn_rm.UseVisualStyleBackColor = true;
+            this.btn_rm.Click += new System.EventHandler(this.btn_rm_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(-1, 503);
+            this.trackBar1.Maximum = 0;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(1008, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(919, 559);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(97, 45);
+            this.trackBar2.TabIndex = 9;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // btn_mou
+            // 
+            this.btn_mou.Location = new System.Drawing.Point(829, 564);
+            this.btn_mou.Name = "btn_mou";
+            this.btn_mou.Size = new System.Drawing.Size(93, 23);
+            this.btn_mou.TabIndex = 10;
+            this.btn_mou.Text = "بی صدا/با صدا";
+            this.btn_mou.UseVisualStyleBackColor = true;
+            this.btn_mou.Click += new System.EventHandler(this.btn_mou_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 661);
+            this.Controls.Add(this.btn_mou);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btn_rm);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_pre);
+            this.Controls.Add(this.btn_play);
+            this.Controls.Add(this.btn_pause);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "مدیا پلیر ~ علی همت نیا";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.Button btn_pre;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button btn_rm;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button btn_mou;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Media Player/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Media_Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            axWindowsMediaPlayer1.uiMode = "none";
+
+        }
+
+        private void btn_mou_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.settings.volume != 0)
+            {
+                axWindowsMediaPlayer1.settings.volume = 0;
+            }
+            else if (axWindowsMediaPlayer1.settings.volume == 0)
+            {
+                axWindowsMediaPlayer1.settings.volume = trackBar2.Value;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog()== DialogResult.OK)
+            {
+                foreach (var filename in openFileDialog1.FileNames)
+                {
+                    axWindowsMediaPlayer1.currentPlaylist.appendItem(axWindowsMediaPlayer1.newMedia(filename));
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                    trackBar1.Maximum = (int)axWindowsMediaPlayer1.currentMedia.duration;
+                    trackBar1.Value = (int)axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+                    timer1.Enabled = true;
+                }
+            }
+        }
+
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            if(axWindowsMediaPlayer1.currentPlaylist.count >= 1)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                trackBar1.Maximum = (int)axWindowsMediaPlayer1.currentMedia.duration;
+                trackBar1.Value = (int)axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+                timer1.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("لطفا فایل را انتخاب کنید");
+            }
+        }
+
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void btn_pre_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.currentPlaylist.count > 1)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.previous();
+                trackBar1.Value = 0;
+            }
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.currentPlaylist.count > 1)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.next();
+                trackBar1.Value = 0;
+            }
+        }
+
+        private void btn_rm_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.currentPlaylist.clear();
+            trackBar1.Maximum = 0;
+            trackBar1.Value = 0;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //axWindowsMediaPlayer1.Ctlcontrols.currentPosition = trackBar1.Value;/
+            
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.volume = trackBar2.Value;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                trackBar1.Maximum = (int)axWindowsMediaPlayer1.currentMedia.duration;
+                trackBar1.Value = (int)axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+            }
+            catch
+            {
+            }
+        }
+    }
+}
